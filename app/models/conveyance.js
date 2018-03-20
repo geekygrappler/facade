64b6ee29ddb39bee6835a1c042b1b826ddcf77c5
Andy Brown <andy.brown@thetrainline.com>
2018-03-20 21:49:37 +0100

    feature(conveyance-model): Create rough outline of a model

---
+++ app/models/conveyance.js
@@ -0,0 +1,31 @@
+import DS from 'ember-data';
+
+/**
+ * Represents a conveyancing case
+ */
+
+export default DS.Model.extend({
+  /**
+   * Represents anything needing to be done for the conveyancing
+   * Searches, Document upload, Agreemeents signed. Many of these will need to be developed
+   * in conjugtion with a solicitor.
+   */
+  tasks: DS.hasMany('task'),
+
+  /**
+   * The user who will be purchasing the property.
+   */
+  buyer: DS.belongsTo('user'),
+
+  /**
+   * The solicitor who will handle the case.
+   */
+  solicitor: DS.belongsTo('user'),
+
+  /**
+   * This is vaguely the address of the property being bought.
+   * Not sure at this stage if we'll need the current address of the buyer as well as
+   * ther property address
+   */
+  address: DS.belongsTo('address')
+});
