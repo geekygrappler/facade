1f9c81befcc5add3674df1afaba4d48aa740b8a0
Andy Brown <andy.brown@thetrainline.com>
2018-03-28 14:56:17 +0200

    model(task)

---
+++ app/models/task.js
@@ -0,0 +1,20 @@
+import DS from 'ember-data';
+
+/**
+ * A task that is part of a conveyancing case
+ * e.g. Passport upload, Church searches
+ */
+
+export default DS.Model.extend({
+  /**
+   * e.g. Passport Upload
+   */
+  title: DS.attr('string'),
+
+  complete: DS.attr('boolean', { defaultValue: false }),
+
+  /**
+   * Helpful information for the buyer to understand this task
+   */
+  description: DS.attr('string')
+});
