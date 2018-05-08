2d0d5b76402b1e732660c91c8c51e4dd760ba532
Andy Brown <brownie3003@gmail.com>
2018-05-09 00:15:02 +0200

    feature(tasks): toggle completeness

--- app/components/conveyance-task.js
+++ app/components/conveyance-task.js
@@ -1,8 +1,18 @@
 import Component from '@ember/component';
 import { conditional } from 'ember-awesome-macros';
 import raw from 'ember-macro-helpers/raw';
+import { task } from 'ember-concurrency';
+import { inject as service } from '@ember/service';
 
 export default Component.extend({
   'data-test-conveyance-task': true,
+  currentUser: service(),
   status: conditional('task.complete', raw('complete'), raw('outstanding')),
+
+  toggleComplete: task(function * () {
+    let task = this.task;
+    let complete = task.complete;
+    task.set('complete', !complete);
+    yield task.save();
+  }).drop(),
 });
--- tests/integration/components/conveyance-task-test.js
+++ tests/integration/components/conveyance-task-test.js
@@ -2,7 +2,8 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
-import { setupFactoryGuy, make } from 'ember-data-factory-guy';
+import { click } from '@ember/test-helpers';
+import { setupFactoryGuy, make, mockUpdate } from 'ember-data-factory-guy';
 
 module('Integration | Component | conveyance-task', function(hooks) {
   setupRenderingTest(hooks);
@@ -39,5 +40,30 @@ module('Integration | Component | conveyance-task', function(hooks) {
     `);
 
     assert.dom('[data-test-description]').containsText('A default description', 'The task\'s description should be displayed');
+  });
+
+  module('for solicitors', function() {
+    test('they can toggle the completed value of the task', async function(assert) {
+      let task = make('task', { complete: false });
+      this.set('task', task);
+      this.owner.lookup('service:currentUser').set('user', make('solicitor'));
+      mockUpdate(task);
+      await render(hbs`
+        {{conveyance-task
+          task=task
+        }}
+      `);
+
+      assert.dom('[data-test-completed-toggle]').containsText('Mark Complete', 'The task toggle button shows correct text when task is incomplete');
+
+      await click('[data-test-completed-toggle]');
+
+      assert.ok(task.complete, 'task should be marked as complete after button is clicked');
+      assert.dom('[data-test-completed-toggle]').containsText('Mark Outstanding', 'The task toggle button shows correct text when task is complete');
+
+      await click('[data-test-completed-toggle]');
+      assert.notOk(task.complete, 'task should be marked as incomplete after button is clicked');
+      assert.dom('[data-test-completed-toggle]').containsText('Mark Complete', 'The task toggle button shows correct text when task is incomplete');
+    });
   });
 });
