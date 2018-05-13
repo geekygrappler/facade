a7c3ef219da4706af848b240f9f8c39a645ee483
Andy Brown <brownie3003@gmail.com>
2018-05-13 18:42:47 +0100

    improve(user model): add fields

--- app/models/user.js
+++ app/models/user.js
@@ -4,6 +4,10 @@ import raw from 'ember-macro-helpers/raw';
 
 export default DS.Model.extend({
   role: DS.attr('string'),
+  prefix: DS.attr('string'),
+  firstName: DS.attr('string'),
+  lastName: DS.attr('string'),
+  email: DS.attr('string'),
 
   isSolicitor: equal('role', raw('solicitor')),
   isBuyer: equal('role', raw('buyer'))
