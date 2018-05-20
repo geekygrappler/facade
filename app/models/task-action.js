019e2533b953a42f7d900e7e382ab456fddd7ca5
Andy Brown <brownie3003@gmail.com>
2018-05-20 17:35:55 +0100

    document(task-action)

--- app/models/task-action.js
+++ app/models/task-action.js
@@ -1,6 +1,15 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
+  /**
+   * The available types as the moment 'document-upload', 'data-entry'
+   */
   type: DS.attr('string'),
+
+  /**
+   * User friendly description of the task
+   * e.g. Identity document (document-upload)
+   * e.g. National Insurance number (data-entry)
+   */
   description: DS.attr('string'),
 });
