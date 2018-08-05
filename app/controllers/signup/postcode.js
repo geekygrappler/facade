0ee190d093e02e9b5ee8691a01c0b87b49ec34af
Andy Brown <brownie3003@gmail.com>
2018-08-05 17:30:45 +0100

    refactor(postcode-template)

--- app/controllers/signup/postcode.js
+++ app/controllers/signup/postcode.js
@@ -1,15 +1,25 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
-import { gt, not } from '@ember/object/computed';
+import { gt, not, and, or } from '@ember/object/computed';
 
 export default Controller.extend({
   signup: service(),
   validPostcode: gt('postcode.length', 4),
   invalidPostcode: not('validPostcode'),
+  validBuyingPostcode: gt('buyingPostcode.length', 4),
+  validSellingPostcode: gt('sellingPostcode.length', 4),
+  validChainPostcodes: and('validBuyingPostcode', 'validSellingPostcode'),
+  invalidChainPostcodes: not('validChainPostcodes'),
+  canProceed: or('validChainPostcodes', 'validPostcode'),
+  cannotProceed: not('canProceed'),
   actions: {
     submit() {
-      this.set('signup.postcode');
-      this.transitionToRoute('signup.quote');
+      if (this.canProceed) {
+        this.set('signup.postcode');
+        this.set('signup.buyingPostcode');
+        this.set('signup.sellingPostcode');
+        this.transitionToRoute('signup.quote');
+      }
     }
   }
 });
