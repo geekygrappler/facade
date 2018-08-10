c9c0db40c32b358f876c92a8a85f70c7e4ad78de
Andy Brown <brownie3003@gmail.com>
2018-08-10 23:26:48 +0200

    refactor(styling): for mobile

--- app/controllers/signup/quote.js
+++ app/controllers/signup/quote.js
@@ -2,5 +2,13 @@ import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
 
 export default Controller.extend({
-  signup: service()
+  signup: service(),
+  actions: {
+    transitionToDeposit() {
+      this.signup.set('firstName', this.firstName);
+      this.signup.set('middleNames', this.middleNames);
+      this.signup.set('lastName', this.lastName);
+      this.transitionToRoute('signup.deposit');
+    }
+  }
 });
