0e7b575a88dcccd42b8c0b3052dab2a4b1fce894
Andy Brown <brownie3003@gmail.com>
2018-08-21 14:45:59 +0200

    route(show): side-load addresses

--- app/routes/conveyances/show.js
+++ app/routes/conveyances/show.js
@@ -3,7 +3,7 @@ import Route from '@ember/routing/route';
 export default Route.extend({
   model(params) {
     return this.get('store').findRecord('conveyance', params.id, {
-      include: 'address,solicitor,buyer,tasks,tasks.buyer-actions,tasks.solicitor-actions',
+      include: 'purchase-address,sale-address,tasks,tasks.buyer-actions,tasks.solicitor-actions',
       reload: true
     });
   },
