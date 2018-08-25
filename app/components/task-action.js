8c2b670bf50e91864cb73d661ad192acc550cac4
Andy Brown <brownie3003@gmail.com>
2018-08-25 21:46:11 +0200

    remove(component): task-action

--- app/components/task-action.js
+++
@@ -1,9 +0,0 @@
-import Component from '@ember/component';
-import { equal } from 'ember-awesome-macros';
-import raw from 'ember-macro-helpers/raw';
-
-export default Component.extend({
-  action: null,
-  isDocumentUpload: equal('action.type', raw('document-upload')),
-  isDataEntry: equal('action.type', raw('data-entry'))
-});
